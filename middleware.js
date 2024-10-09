f2865ed8daca072f2552365c26747150f3aa3dc2
Krish Yadav <44264836+krishyadav007@users.noreply.github.com>
2024-10-09 13:56:54 +0530

    Had forogot uncommenting

--- middleware.js
+++ middleware.js
@@ -4,18 +4,15 @@ import { auth } from "@/auth"
 export async function middleware(request) {
     const session = await auth();
     console.log(session)
-//   if (request.nextUrl.pathname.startsWith('/api')) {
-//     if(!(session !== null)) {
-//         return new Response(JSON.stringify({ message: "You are not authorized to view this " }), {
-//           status: 401,
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//         });
-//     }
-//   }
- 
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.rewrite(new URL('/dashboard/user', request.url))
+  if (request.nextUrl.pathname.startsWith('/api')) {
+    if(!(session !== null)) {
+        return new Response(JSON.stringify({ message: "You are not authorized to view this " }), {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+    }
   }
+
 }
